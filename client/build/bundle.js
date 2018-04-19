5fa4418ad3f26e827961d9c95f9862ef49f22cea
Victor Cintron <hotslug@gmail.com>
2018-04-19 13:47:54 +0100

    Adds app.js

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -65,9 +65,415 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Request = __webpack_require__(1);
+const CountryView = __webpack_require__(2);
+const MapWrapper = __webpack_require__(3);
+
+const allCountriesRequest = new Request("https://restcountries.eu/rest/v2/all")
+const dbrequest = new Request("/api/countries");
+
+const countryView = new CountryView();
+
+
+
+const getCountryDetails = function(country) {
+  const countryDetails = {
+    name: country.name,
+    population: country.population,
+    capital: country.capital,
+    flag: country.flag,
+    latlng: country.latlng
+  };
+
+  return countryDetails;
+}
+
+const populateCountriesList = function(allCountries) {
+
+  const countrySelector = document.getElementById('select-country');
+
+  for (let country of allCountries) {
+    const option = document.createElement('option');
+    const countryDetails = getCountryDetails(country);
+    option.value = JSON.stringify(countryDetails); // value is JSON object
+    option.innerText = country.name;
+    countrySelector.appendChild(option);
+  }
+
+}
+
+const populateBucketList = function(countries) {
+  countries.forEach(country => countryView.addCountry(country));
+}
+
+
+const saveCountry = function() {
+  const countrySelector = document.getElementById('select-country');
+  const selectedCountryJSON = countrySelector.value;
+  const selectedCountryObj = JSON.parse(selectedCountryJSON);
+
+  dbrequest.post(saveRequestComplete, selectedCountryObj);
+}
+
+const saveRequestComplete = function(countryToSave) {
+  countryView.addCountry(countryToSave);
+}
+
+
+const initializeMap = function() {
+  const container = document.getElementById("bucket-map");
+  const center = { lat: 0, lng: 0 };
+  const zoom = 2;
+  const map = new MapWrapper(container, center, zoom);
+};
+
+
+
+
+const clearBucketList = function() {
+  dbrequest.delete(countryView.clearList);
+}
+
+
+const app = function() {
+  allCountriesRequest.get(populateCountriesList);
+  initializeMap();
+  dbrequest.get(populateBucketList)
+
+
+  const selectCountryButton = document.getElementById('select-country-button');
+  selectCountryButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    saveCountry();
+  });
+
+  const selectClearButton = document.getElementById('clear-list');
+  selectClearButton.addEventListener('click', clearBucketList)
+};
+
+
+
+document.addEventListener('DOMContentLoaded', app);
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
-throw new Error("Module parse failed: Unexpected token (5:0)\nYou may need an appropriate loader to handle this file type.\n| const MapWrapper = require('./views/mapWrapper.js');\n| \n| <<<<<<< HEAD\n| const countriesRequest = new Request(\"https://restcountries.eu/rest/v2/all\")\n| =======");
+const Request = function(url){
+  this.url = url;
+}
+
+Request.prototype.get = function(callback){
+  const request = new XMLHttpRequest();
+  request.open("GET", this.url);
+  request.addEventListener("load", function(){
+    if(this.status !==200){
+      return;
+    }
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  });
+  request.send();
+};
+
+Request.prototype.post = function(callback, body){
+  const request = new XMLHttpRequest();
+  request.open("POST", this.url);
+  request.setRequestHeader("Content-Type", "application/json");
+
+  request.addEventListener("load", function(){
+    if(this.status !== 201){
+      return;
+    }
+
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  });
+  request.send(JSON.stringify(body));
+};
+
+Request.prototype.delete = function(callback){
+  const request = new XMLHttpRequest();
+  request.open("DELETE", this.url);
+  request.addEventListener("load", function(){
+    if(this.status !==204){
+      return;
+    }
+    callback();
+  });
+  request.send();
+};
+
+Request.prototype.removeCountryFromList = function (countryID) {
+};
+
+
+module.exports = Request;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+const CountryView = function(){
+  this.countries = [];
+}
+
+CountryView.prototype.addCountry = function (country) {
+  this.countries.push(country);
+  this.render(country);
+};
+
+CountryView.prototype.render = function (country) {
+  const bucketList = document.getElementById('bucket-list');
+
+  const li = this.formatCountryData(country);
+  bucketList.appendChild(li);
+};
+
+CountryView.prototype.formatCountryData = function (country) {
+    const li = document.createElement('li');
+    country.className = 'country-list-item';
+    // name
+    const name = document.createElement('p');
+    name.className = 'countryName';
+    name.innerText = country.name;
+    // capital
+    const capital = document.createElement('p');
+    capital.innerText = `Capital: ${country.capital}`;
+    // population
+    const pop = document.createElement('p');
+    pop.innerText = `Population: ${country.population}`;
+    // flag
+    const flag = document.createElement('img');
+    flag.className = 'flag-img';
+    flag.src = country.flag;
+
+
+    [name, capital, pop, flag].forEach(element => li.appendChild(element));
+
+    return li;
+};
+
+CountryView.prototype.clearList = function () {
+  this.countries = [];
+  const bucketList = document.getElementById('bucket-list');
+  bucketList.innerHTML = "";
+};
+
+module.exports = CountryView;
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+
+
+const MapWrapper = function (container, coords, zoom) {
+  this.googleMap = new google.maps.Map(container, {
+    center: coords,
+    zoom: zoom,
+    style: [
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#00a7b5"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ef7a7d"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d6d6d6"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#e3e3e3"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d7d7d7"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#595959"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "color": "#c6c6c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#00a7b5"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 700
+            },
+            {
+                "color": "#999999"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#7dcdcd"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#bbcfe3"
+            }
+        ]
+    }
+]
+
+  });
+}
+
+MapWrapper.prototype.addMarker = function (coords) {
+  var bucketImage = {
+    url: 'bucket.svg',
+    scaledSize: new google.maps.Size(35, 35)
+};
+
+  var marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap,
+    icon: bucketImage,
+    animation: google.maps.Animation.DROP
+  });
+  return marker;
+}
+
+MapWrapper.prototype.addClickEvent = function () {
+  google.maps.event.addListener(this.googleMap, 'click', function (event) {
+    var position = { lat: event.latLng.lat(), lng: event.latLng.lng() }
+    this.addMarker(position);
+  }.bind(this));
+}
+
+MapWrapper.prototype.addInfoWindow = function (coords, text) {
+  var marker = this.addMarker(coords);
+  marker.addListener('click', function () {
+    var infoWindow = new google.maps.InfoWindow({
+      content: text
+    });
+    infoWindow.open(this.map, marker);
+  });
+}
+
+  // Make Map
+
+MapWrapper.prototype.initializeMap = function(country) {
+    const mapDiv = document.getElementById('bucket-map');
+    const center = { lat: country.lat, lng: country.lng };
+    const mainMap = new MapWrapper(mapDiv, center, 16);
+
+    mainMap.addInfoWindow(center, `<h2>${country.name}</h2>`); //+
+  }
+
+
+MapWrapper.prototype.geoLocate = function () {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var center = { lat: position.coords.latitude, lng: position.coords.longitude };
+    this.googleMap.setCenter(center);
+    this.addMarker(center);
+  }.bind(this));
+}
+
+module.exports = MapWrapper;
+
 
 /***/ })
 /******/ ]);
--- client/src/app.js
+++ client/src/app.js
@@ -2,11 +2,7 @@ const Request = require('./services/request.js');
 const CountryView = require('./views/countryView.js');
 const MapWrapper = require('./views/mapWrapper.js');
 
-<<<<<<< HEAD
-const countriesRequest = new Request("https://restcountries.eu/rest/v2/all")
-=======
 const allCountriesRequest = new Request("https://restcountries.eu/rest/v2/all")
->>>>>>> master
 const dbrequest = new Request("/api/countries");
 
 const countryView = new CountryView();
@@ -23,50 +19,6 @@ const getCountryDetails = function(country) {
   };
 
   return countryDetails;
-<<<<<<< HEAD
-}
-
-const populateList = function(countries) {
-
-  const selector = document.getElementById('select-country');
-
-  for (let country of countries) {
-    const option = document.createElement('option');
-    const countryDetails = getCountryDetails(country);
-    option.value = JSON.stringify(countryDetails); // value is JSON object
-    option.innerText = country.name;
-    selector.appendChild(option);
-  }
-
-}
-
-const populateBucketList = function(countries) {
-  countries.forEach(country => countryView.addCountry(country));
-}
-
-
-const saveCountry = function() {
-  const countrySelector = document.getElementById('select-country');
-  const selectedCountryJSON = countrySelector.value;
-  const selectedCountryObj = JSON.parse(selectedCountryJSON);
-
-  dbrequest.post(saveRequestComplete, selectedCountryObj);
-}
-
-const saveRequestComplete = function(countryToSave) {
-  countryView.addCountry(countryToSave);
-}
-
-
-const initializeMap = function() {
-  const container = document.getElementById("bucket-map");
-  const center = { lat: 0, lng: 0 };
-  const zoom = 2;
-  const map = new MapWrapper(container, center, zoom);
-};
-const countrySelected = function(event){
-  const country = document.querySelector("#option").value;
-=======
 }
 
 const populateCountriesList = function(allCountries) {
@@ -80,7 +32,6 @@ const populateCountriesList = function(allCountries) {
     option.innerText = country.name;
     countrySelector.appendChild(option);
   }
->>>>>>> master
 
 }
 
@@ -90,7 +41,7 @@ const populateBucketList = function(countries) {
 
 
 const saveCountry = function() {
-  const countrySelector = document.getElementById('country-select');
+  const countrySelector = document.getElementById('select-country');
   const selectedCountryJSON = countrySelector.value;
   const selectedCountryObj = JSON.parse(selectedCountryJSON);
 
@@ -102,8 +53,6 @@ const saveRequestComplete = function(countryToSave) {
 }
 
 
-<<<<<<< HEAD
-=======
 const initializeMap = function() {
   const container = document.getElementById("bucket-map");
   const center = { lat: 0, lng: 0 };
@@ -114,26 +63,16 @@ const initializeMap = function() {
 
 
 
->>>>>>> master
 const clearBucketList = function() {
   dbrequest.delete(countryView.clearList);
 }
 
 
-<<<<<<< HEAD
-const appStart = function() {
-  countriesRequest.get(populateList);
-  initializeMap();
-  dbrequest.get(populateBucketList);
-
-}
-=======
 const app = function() {
   allCountriesRequest.get(populateCountriesList);
   initializeMap();
   dbrequest.get(populateBucketList)
 
->>>>>>> master
 
   const selectCountryButton = document.getElementById('select-country-button');
   selectCountryButton.addEventListener('click', function(event) {
