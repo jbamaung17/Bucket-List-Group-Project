a8a6823e93801ca01d62f2579f1cd4f729779c29
Victor Cintron <hotslug@gmail.com>
2018-04-19 09:55:18 +0100

    Adds files.

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -76,7 +76,7 @@ const requestCountries = new Request('https://restcountries.eu/rest/v2/all');
 
 
 const getCountriesComplete = function(countries) {
-  countryView.createCountryDropdown(countries);
+  bucketView.populateSelect(countries);
 }
 
 
@@ -84,10 +84,7 @@ const appStart = function() {
   console.log("DOM content loaded, app starting... ");
   requestCountries.get(getCountriesComplete);
 
-  const dropDown = document.querySelector("#select-country");
-  dropDown.addEventListener("change", function(event) {
-    console.log(event);
-  })
+
 
   // Make Map
 
@@ -95,7 +92,6 @@ const appStart = function() {
     const mapDiv = document.getElementById('bucket-map');
     const center = { lat: country.lat, lng: country.lng };
     const mainMap = new MapWrapper(mapDiv, center, 16);
-
 
     mainMap.addInfoWindow(center, `<h2>${country.name}</h2>`); //+
   }
@@ -119,9 +115,10 @@ Request.prototype.get = function(callback){
     if(this.status !== 200) {
       return;
     }
-    const responseBody = json.parse(this.responseText);
+    const responseBody = JSON.parse(this.responseText);
     callback(responseBody);
-  })
+  });
+
   request.send();
 };
 
@@ -159,38 +156,71 @@ module.exports = Request;
 /* 2 */
 /***/ (function(module, exports) {
 
-var BucketView = function(){
+var BucketView = function() {
   this.countries = [];
 }
 
-BucketView.prototype.addCountry = function(country){
+BucketView.prototype.addCountry = function(country) {
   this.countries.push(country);
   this.render(country);
 };
 
-BucketView.prototype.clear = function(country){
+BucketView.prototype.clear = function(country) {
   this.countries = [];
   const ul = document.querySelector("#countries");
   ul.innerHTML = '';
 };
 
-BucketView.prototype.createCountryDropdown = function (countries) {
-  const select = document.getElementById('select-country');
-  for (let country of countries) {
-    const option = document.createElement('option');
-    option.value = country;
-    option.innerText = country.name;
-    select.appendChild(option);
-  }
-};
+BucketView.prototype.populateSelect = function(countries) {
+  const select = document.getElementById('country-select')
+  countries.forEach(function(country, index) {
+    let option = document.createElement('option')
+    option.innerText = country.name
+    option.value = index
+    select.appendChild(option)
+  })
+  getCountry(countries);
+}
 
-BucketView.prototype.render = function(country){
-  const ul = document.querySelector("#countries");
-  const li = document.createElement('li');
-  const text = document.createElement('p');
-  text.innerText = `${country.name}`;
-  li.appendChild(text);
-  ul.appendChild(li);
+const getCountry = function(countries) {
+  const selectedCountry = document.querySelector('select')
+  selectedCountry.addEventListener('change', function() {
+    let country = countries[this.value]
+  })
+}
+
+BucketView.prototype.countryDetails = function(country) {
+  const div = document.getElementById('country-list')
+  clearContent(div)
+  const divCountry = document.createElement('div')
+  divCountry.classList.add('country-div')
+  const divImage = document.createElement('div')
+  divImage.classList.add('image')
+  const divDetails = document.createElement('div')
+  divDetails.classList.add('div-details')
+  const countryName = document.createElement('h3')
+  countryName.innerText = country.name
+  const countryImage = document.createElement('img')
+  countryImage.src = country.flag
+  countryImage.alt = country.name
+  countryImage.classList.add('country_image')
+  div.appendChild(divCountry)
+  divCountry.appendChild(divImage)
+  divCountry.appendChild(divDetails)
+  divImage.appendChild(countryImage)
+  divDetails.appendChild(countryName)
+  return div
+}
+
+const saveCountry = function(country) {
+  const jsonString = JSON.stringify(country);
+  localStorage.setItem('currentcountry', jsonString);
+}
+
+const clearContent = function(node) {
+  while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+  }
 }
 
 module.exports = BucketView;
