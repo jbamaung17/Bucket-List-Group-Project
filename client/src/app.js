e35f52e3eb4ec8858155505a3e2b3bbe0129c119
Victor Cintron <hotslug@gmail.com>
2018-04-19 10:10:55 +0100

    Removes app from app.js.

--- client/src/app.js
+++ client/src/app.js
@@ -15,17 +15,4 @@ const appStart = function() {
   console.log("DOM content loaded, app starting... ");
   requestCountries.get(getCountriesComplete);
 
-
-
-  // Make Map
-
-  const initializeMap = function(country) {
-    const mapDiv = document.getElementById('bucket-map');
-    const center = { lat: country.lat, lng: country.lng };
-    const mainMap = new MapWrapper(mapDiv, center, 16);
-
-    mainMap.addInfoWindow(center, `<h2>${country.name}</h2>`); //+
-  }
-}
-
 document.addEventListener('DOMContentLoaded', appStart);
